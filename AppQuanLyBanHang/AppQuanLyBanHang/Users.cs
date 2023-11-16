97630d8bd39ddf4acd61d6cb882c0f14309641aa
hhuuduc <huynhhuuduc5031@gmail.com>
2023-11-16 11:27:28 +0700

    add project first

---
+++ AppQuanLyBanHang/AppQuanLyBanHang/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppQuanLyBanHang
+{
+    class Users
+    {
+        private int _idUsers;
+
+        private string _matKhau;
+
+        public Users()
+        {
+
+        }
+        public Users(int IdUsers, string matKhau)
+        {
+            this._idUsers = IdUsers;
+            this._matKhau = matKhau;
+        }
+        public int TenUsers { get => _idUsers; set => _idUsers = value; }
+        public string MatKhau { get => _matKhau; set => _matKhau = value; }
+    }
+}
---
+++ WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanHang.Context;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        WebsiteBanHangEntities7 ObjWebsiteBanHangEntities = new WebsiteBanHangEntities7();
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            var lstProduct = ObjWebsiteBanHangEntities.Products.ToList();
+
+            return View(lstProduct);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+          
+           return View();
+
+        }
+    }
+}
